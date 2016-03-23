9d985999f7a2c6ef59c6aa3e236978d8c164e563
Nate Jones <nate@endot.org>
2016-03-23 10:08:33 -0700

    add run command

---
+++ run.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type RunCommand struct {
+	CreateCommand
+	Remove bool `long:"rm" description:"Remove environment after disconnecting."`
+}
+
+func (ccommand *RunCommand) toCreateOpts(sc SystemClient, workingDir string) CreateOpts {
+	return CreateOpts{
+		Name:       ccommand.Args.Name,
+		ProjectDir: ccommand.Directory,
+		Ports:      ccommand.Ports,
+		Volumes:    ccommand.Volumes,
+		WorkingDir: workingDir,
+		Build: BuildOpts{
+			Type:     ccommand.Type,
+			Version:  ccommand.Version,
+			Image:    ccommand.Image,
+			Username: sc.Username(),
+			UID:      sc.UID(),
+			GID:      sc.GID(),
+		},
+	}
+}
+
+var runCommand RunCommand
+
+func (x *RunCommand) Execute(args []string) error {
+	dc, err := NewDockerClient(globalOptions.toConnectOpts())
+	if err != nil {
+		return err
+	}
+
+	sc, err := NewSystemClient()
+	if err != nil {
+		return err
+	}
+
+	workingDir, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	err = CreateEnvironment(dc, sc, runCommand.toCreateOpts(sc, workingDir), os.Stdout)
+	if err != nil {
+		return err
+	}
+
+	err = ConnectEnvironment(dc, sc, runCommand.Args.Name, connectCommand.Args.Rest)
+	if err != nil {
+		return err
+	}
+
+	if runCommand.Remove {
+		err = DestroyEnvironment(dc, sc, runCommand.Args.Name)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func init() {
+	_, err := parser.AddCommand("run",
+		"Run an environment.",
+		"",
+		&runCommand)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+}
