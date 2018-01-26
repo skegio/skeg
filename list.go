cfa1e51d4b24bd8cbc506827630f77b4274ee728
Nate Jones <nate@endot.org>
2018-01-26 11:40:36 -0800

    add ls alias for list

--- list.go
+++ list.go
@@ -57,10 +57,12 @@ func listEnvironments(envs map[string]Environment) error {
 }
 
 func init() {
-	_, err := parser.AddCommand("list",
+	cmd, err := parser.AddCommand("list",
 		"List environments.",
 		"",
 		&listCommand)
+
+	cmd.Aliases = append(cmd.Aliases, "ls")
 
 	if err != nil {
 		fmt.Println(err)
